cef0f9a125f434c62560ee70ffeccea73c82d680
JR-espacial <jorge_alan_2001@hotmail.com>
2023-08-09 14:45:23 -0400

    Fix authentication

--- final_project/index.js
+++ final_project/index.js
@@ -27,42 +27,7 @@ app.use("/customer/auth/*", function auth(req,res,next){
      }
 });
 
-app.post("/login", (req,res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-  
-    if (!username || !password) {
-        return res.status(404).json({message: "Error logging in"});
-    }
-   if (authenticatedUser(username,password)) {
-      let accessToken = jwt.sign({
-        data: password
-      }, 'access', { expiresIn: 60 * 60 });
-  
-      req.session.authorization = {
-        accessToken,username
-    }
-    return res.status(200).send("User successfully logged in");
-    } else {
-      return res.status(208).json({message: "Invalid Login. Check username and password"});
-}});
 
-
-app.post("/register", (req,res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-  
-    if (username && password) {
-      if (!doesExist(username)) { 
-        users.push({"username":username,"password":password});
-        return res.status(200).json({message: "User successfully registred. Now you can login"});
-      } else {
-        return res.status(404).json({message: "User already exists!"});    
-      }
-    } 
-    return res.status(404).json({message: "Unable to register user."});
-});
- 
 const PORT =3000;
 
 app.use("/customer", customer_routes);
