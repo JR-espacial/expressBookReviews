4d6871b8d421824b614462478d03a1e1bda5ebb6
JARE2001 <A01701350@itesm.mx>
2023-08-09 23:41:28 -0600

    add async await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,58 +5,98 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+async function fetchBooks() {
+    // Simulating a delay, like fetching data from a database
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return books; // assuming books is a global variable or available in scope
+}
+
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        if (!isValid(username)) { 
+            users.push({"username":username,"password":password});
+            return res.status(200).json({message: "User successfully registred. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});    
+        }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    return res.send(JSON.stringify(books))
+public_users.get('/',async function (req, res) {
+    try {
+        const result = await fetchBooks();
+        res.send(JSON.stringify(result));
+    } catch (error) {
+        res.status(500).send('Error fetching books');
+    }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Im asuming the fist number is the ISBN as there no other such field  
-  return res.send(books[req.params.isbn]);
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    const arrBooks = Object.values(books)
+public_users.get('/isbn/:isbn',async function (req, res) {
+    //Im asuming the fist number is the ISBN as there no other such field  
+    try {
+        const result = await fetchBooks();
+        res.send(result[req.params.isbn]);
+    } catch (error) {
+        res.status(500).send('Error fetching books by isbn');
+    }
+});
 
-    let found = false
-    arrBooks.forEach(book=>{
-        if(book.author == req.params.author){
-            found =true
-            return res.send(book);
+// Get book details based on author
+public_users.get('/author/:author', async function (req, res) {
+    try {
+        const arrBooks = Object.values( await fetchBooks())
+    
+        let found = false
+        arrBooks.forEach(book =>{
+            if(book.author == req.params.author){
+                found =true
+                return res.send(book);
+            }
+        });
+        if(!found){
+            return res.send("Not found")
         }
-    });
-    if(!found){
-        return res.send("Not found")
+    } catch (error) {
+        res.status(500).send('Error fetching books by author');
     }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    const arrBooks = Object.values(books)
+public_users.get('/title/:title',async function (req, res) {
 
-    let found = false
-    arrBooks.forEach(book=>{
-        if(book.title == req.params.title){
-            found =true
-            return res.send(book);
+    try {
+        const arrBooks = Object.values( await fetchBooks())
+    
+        let found = false
+        arrBooks.forEach(book=>{
+            if(book.title == req.params.title){
+                found =true
+                return res.send(book);
+            }
+        });
+        if(!found){
+            return res.send("Not found")
         }
-    });
-    if(!found){
-        return res.send("Not found")
+    } catch (error) {
+        res.status(500).send('Error fetching books by title');
     }
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Im asuming the fist number is the ISBN as there no other such field  
-  return res.send(books[req.params.isbn]);
+public_users.get('/review/:isbn',async function (req, res) {
+  //Im asuming the first number is the ISBN as there no other such field  
+    try {
+        const result = await fetchBooks();
+        res.send(result[req.params.isbn].reviews);
+    } catch (error) {
+        res.status(500).send('Error fetching book reviews by isbn');
+    }
 });
 
 module.exports.general = public_users;
